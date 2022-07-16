547352be5b94df7964886b8d33f64988bce58af5
jangismarifet <jangismarifet@gmail.com>
2022-07-16 21:05:17 +0300

    Completed Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,10 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import howManyParks from "./parks/howManyParks";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
-
+  <MesaVerde />
   return <h1>Colorado State Parks!</h1>;
 }
